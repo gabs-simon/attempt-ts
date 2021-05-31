2c021d25c142082d113fd386bf4772185c64d83e
Gabs <gabs.simon@outlook.com>
2021-05-30 18:10:59 -0700

    Fixed repo reference

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -7,8 +7,8 @@ module.exports = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'gabs-simon', // Usually your GitHub org/user name.
+  projectName: 'attempt-ts', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'Attempt-TS',
