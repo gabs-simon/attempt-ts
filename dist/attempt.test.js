d23215d06d74ec9308028973c7aeaea62b97ee97
Gabs <gabs.simon@outlook.com>
2021-05-31 00:33:05 -0700

    feat(ci): Generate artifacts, release notes and changelog

--- dist/attempt.test.js
+++ dist/attempt.test.js
@@ -5,12 +5,12 @@ const failure_1 = require("./failure");
 const attempt_1 = require("./attempt");
 describe('Attempt', () => {
     it('Should correctly return value given Success', () => {
-        const testFunction = (a) => ((a > 0) ? success_1.Success('yes') : failure_1.Failure(0));
+        const testFunction = (a) => a > 0 ? success_1.Success('yes') : failure_1.Failure(0);
         const result = testFunction(10);
         expect(attempt_1.Attempt(result)).toBe('yes');
     });
     it('Should correctly throw an exception given failure', () => {
-        const testFunction = (a) => (a > 0 ? success_1.Success('yes') : failure_1.Failure(0));
+        const testFunction = (a) => a > 0 ? success_1.Success('yes') : failure_1.Failure(0);
         const result = testFunction(-1);
         try {
             attempt_1.Attempt(result);
